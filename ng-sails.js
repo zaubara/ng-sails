2297b7b591ffabbb7b5a2f2f875d571fd74b0127
Martin Puza <m.puza@zetes.at>
2015-04-13 18:09:50 +0200

    added suffix, first steps to handle non-array objects

--- ng-sails.js
+++ ng-sails.js
@@ -1,116 +1,126 @@
-angular.module('ngSails', []).factory('$sails', ['$q','$rootScope',
-	function($q,$rootScope) {
+angular.module('ngSails', []).factory('$sails', ['$q',
+	function($q) {
+		var $sails = function(model, scope, params, prefix, suffix) {
+			if (!io) throw "Can't see socket.io in the global scope?";
+			if(prefix && typeof prefix != 'undefined')
+				this.prefix = '/'+prefix+'/';
+			else
+				this.prefix = '/';
 
-		var $sails = function(sailsmodel, angularmodel, $scope) {
-			this.length = 0;
-			(function(sailsmodel, angularmodel, $scope) {
-				if (!sailsmodel && !angularmodel) return;
-				if (!$scope) {
-					$scope = angularmodel;
-					angularmodel = sailsmodel;
-				}
-				this.api = sailsmodel;
-				this.model = angularmodel;
-				this.hooks = {
-					onfetch:[],
-					onsubscribe:[]
-				};
-				this.params = {
-					limit:30,
-					skip:0,
-					sort:'id desc'
-				};
-				this.scope = $scope;
-				this.fetch(true);
-			}.bind(this))(sailsmodel, angularmodel, $scope);
-		}
+			if(suffix && typeof suffix != 'undefined')
+				this.suffix = '/' + suffix;
+			else
+				this.suffix = '';
 
-		$sails.prototype = [];
-		if (new $sails().length !== 0) {
-			$sails.prototype = {};
-			["join","pop","push","reverse","shift","slice","sort","splice","unshift","concat"].forEach(function(m) {
-				$sails.prototype[m] = Array.prototype[m];
-			});
-		}
-		$sails.prototype.toString = Object.prototype.toString;
-		$sails.prototype.concat = function () { return Array.prototype.concat.apply(this.slice(), arguments);};
+			this.model = model || '';
+			this.scope = scope || {};
+			this.params = {
+				limit:30,
+				skip:0,
+				sort:'id desc'
+			}
+			if (params) angular.extend(this.params,params);
+			this.data = {};
+			this.hardFetch(true);
+		};
+
 		$sails.prototype.responseHandler = function(response) {
-			this.scope[this.model] = this.scope[this.model] || [];
-			switch(response.verb) {
+			switch (response.verb) {
 				case "created":
-					if (this.params.sort === 'id desc' && this.params.skip === 0) {
-						this.scope[this.model].unshift(response.data);
-						if (this.scope[this.model].length > ~~this.params.limit) this.scope[this.model] = this.scope[this.model].slice(0,Math.max(this.params.limit,0));
+					if (this.params.sort === "id desc" && this.params.skip === 0) {
+						this.scope[this.model].data.unshift(response.data);
+						if (this.scope[this.model].data.length > ~~this.params.limit) {
+							this.scope[this.model].data = this.scope[this.model].slice(0,Math.max(this.params.limit,0));
+						}
 					}
 					break;
-
 				case "updated":
-					this.scope[this.model].forEach(function(item) {
-						if (item.id == response.id) angular.extend(item,response.data);
+					this.scope[this.model].data.forEach(function(object) {
+						if (object.id == response.id) {
+							angular.extend(object,response.data);
+						}
 					}.bind(this));
 					break;
-
 				case "destroyed":
-					var performFetch = false;
-					this.scope[this.model].forEach(function(item,key) {
-						if (item.id == response.id) performFetch = true;
+					var update = false;
+					this.scope[this.model].data.forEach(function(object,key) {
+						if (object.id == response.id) {
+							this.scope[this.model].data.splice(key,1);
+							update = true;
+						}
 					}.bind(this));
-					if (performFetch) this.fetch(false);
+					if (update) this.hardFetch();
 					break;
 			}
 			this.scope.$apply();
 			return this;
 		};
-		$sails.prototype.fetch = function(subscribe) {
-			if (!this.api) return;
-			io.socket.request((this.api.substr(0,1)!=="/"?'/':'')+this.api, this.params, function(response) {
-				this.scope[this.model].length = 0;
-				Array.prototype.push.apply(this.scope[this.model], response);
-				this.scope.$apply();
+
+		$sails.prototype.crud = function(method, object, id) {
+			var q = $q.defer();
+			object = object || {};
+			method = (typeof method == "string" ? method : 'get').toLowerCase();
+			io.socket[method](this.prefix+this.model+this.suffix+(id ? '/'+id : ''), object, function(data,response) {
+				var verb = false;
+				switch (method) {
+					case "post":
+						verb = "created";
+						break;
+					case "put":
+						verb = "updated";
+						break;
+					case "delete":
+						verb = "destroyed"
+						break;
+				}
+				console.log(verb, id, data);
+				if (verb) this.responseHandler({verb:verb, id:data.id, data:data});
+				if (response.statusCode == 200) {
+					q.resolve(data);
+				} else {
+					q.reject(data);
+				}
+			}.bind(this));
+			return q.promise;
+		}
+
+		$sails.prototype.hardFetch = function(subscribe) {
+			io.socket.request(this.prefix+this.model+this.suffix, this.params, function(response) {
+				console.log(this.prefix, this.model, this.params, response);
+				if ('function' === typeof response.reverse) {
+					//console.log('reverse called');
+					response.reverse();
+					this.scope[this.model].data = response;
+				} else {
+					//console.log('reverse NOT called');
+					this.scope[this.model].data = [response];
+				}
 				if (subscribe) this.subscribe();
-				this.hooks.onfetch.forEach(function(hook) { hook(); });
+				this.scope.$apply();
 			}.bind(this));
 			return this;
 		};
+
 		$sails.prototype.subscribe = function() {
 			io.socket.on(this.model, this.responseHandler.bind(this));
-			this.hooks.onsubscribe.forEach(function(hook) { hook(); });
-			return this;
-		}
-		$sails.prototype.where = function(where) {
-			if (!this.params) this.params = this.defaultParams();
-			this.params.where = where?where:{};
-			this.fetch(false);
 			return this;
 		};
-		$sails.prototype.crud = function(method, object, id) {
-			var q = $q.defer(),
-				object = object || {},
-				method = (typeof method === "string"?method:"get").toLowerCase();
-				io.socket[method](this.api+(id?'/'+id:''),object,function(data,response) {
-					var verb = false;
-					switch (method) {
-						case "post":
-							verb = "created";
-							break;
-						case "put":
-							verb = "updated";
-							break;
-						case "delete":
-							verb = "destroyed";
-							break;
-					}
-					if (verb) this.responseHandler({verb:verb, id:data.id, data:data});
-					if (response.status == 200) return q.resolve(data);
-					q.reject(data);
-				}.bind(this));
-			return q.promise;
-		};
-		$sails.prototype.create = function(object) { return this.crud('post', object); };
-		$sails.prototype.update = function(id, object) { return this.crud('put', object, id); };
-		$sails.prototype.destroy = function(id, object) { return this.crud('delete', object, id); };
-		$sails.prototype.retrieve = function(id, object) { return this.crud('get', object, id); };
-		return $sails;
 
+		$sails.prototype.create = function(object) {
+			return this.crud('post', object);
+		};
+
+		$sails.prototype.update = function(id, object) {
+			return this.crud('put', object, id);
+		};
+
+		$sails.prototype.destroy = function(id, object) {
+			return this.crud('delete', object, id);
+		};
+
+		$sails.prototype.retrieve = function(id, object) {
+			return this.crud('get', object, id);
+		};
+		return $sails;
 	}
 ]);
